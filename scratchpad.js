90e225e213ee6ec0c1914f15fef4c9ec28075ebd
Lukacs Berki <github@lberki.net>
2016-12-28 22:08:18 +0100

    Initial commit

---
+++ scratchpad.js
@@ -0,0 +1,204 @@
+"use strict";
+
+// - Figure out how to update the web page quickly during development
+// - Tweak timeouts (e.g. service worker staling) for production
+// - Figure out if we what prod and dev to be any different
+let itemIds = [];
+let selectedItem = null;
+let selectedId = null;
+
+let changeTimeoutId = null;
+
+let UPDATE_TIMEOUT = 2000;
+let WHITESPACE_REGEX = /^\s*$/;
+
+function log(s) {
+  console.log(new Date().toISOString() + " " + s);
+}
+
+function generateId() {
+  let digits = "0123456789abcdef";
+  let result = "";
+  for (let i = 0; i < 32; i++) {
+    result = result + digits[Math.floor(Math.random() * 16)];
+  }
+
+  return result;
+}
+
+function updateDatabaseFromChange() {
+  log("update");
+  markChangesAsCommitted();
+  selectedItem.title = document.querySelector(".title").value;
+  selectedItem.content = document.querySelector(".content").value;
+  localStorage.setItem("item:" + selectedId, JSON.stringify(selectedItem));
+  refreshItemList();
+}
+
+function itemChanged() {
+  if (changeTimeoutId !== null) {
+    clearTimeout(changeTimeoutId);
+  }
+
+  changeTimeoutId = setTimeout(updateDatabaseFromChange, UPDATE_TIMEOUT);
+  document.querySelector(".changed_marker").style.visibility = "visible";
+}
+
+function itemKeyDown(id, e) {
+  let currentIndex = itemIds.indexOf(id);
+  let newIndex;
+  if (e.keyCode == 13) {
+    log("enter 3 " + id);
+    selectItem(id);
+    e.preventDefault();
+  }
+  
+  if (e.keyCode == 40) {
+    log("down " + id);
+    newIndex = currentIndex + 1;
+  } else if (e.keyCode == 38) {
+    log("up " + id);
+    newIndex = currentIndex - 1;
+  } else {
+    return;
+  }
+  
+  if (newIndex >= 0 && newIndex < itemIds.length) {
+    log(newIndex);
+    document.querySelector(".items_pane").childNodes[newIndex].focus();
+  }
+}
+
+function createListItem(id, item, tabIndex) {
+  let result = document.createElement("div");
+  // Theoretically, we should check if there are any
+  result.textContent = item.title.match(WHITESPACE_REGEX) ? "(empty)" : item.title;
+  result.className = id == selectedId ? "item selected_item" : "item";
+  result.setAttribute("tabindex", tabIndex);
+  result.addEventListener("click", () => { selectItem(id); });
+  result.addEventListener("keydown", e => { itemKeyDown(id, e); });
+  return result;
+}
+
+function getListElementById(id) {
+  return document.querySelector(".items_pane").childNodes[itemIds.indexOf(id)];
+}
+
+function markChangesAsCommitted() {
+  clearTimeout(changeTimeoutId);
+  changeTimeoutId = null;
+  document.querySelector(".changed_marker").style.visibility = "hidden";
+}
+function selectItem(id) {
+  if (id === selectedId) {
+    return;
+  }
+  
+  if (changeTimeoutId !== null) {
+    updateDatabaseFromChange();
+  }
+  if (selectedId !== null) {
+    let currentElement = getListElementById(selectedId);
+    currentElement.className = "item";
+  }
+  
+  if (id !== null && itemIds.indexOf(id) >= 0) {
+    let newElement = document.querySelector(".items_pane").childNodes[itemIds.indexOf(id)];
+    newElement.className = "item selected_item";
+    selectedItem = JSON.parse(localStorage.getItem("item:" + id));
+    selectedId = id;
+    updateContent();
+
+    let contentElement = document.querySelector(".content");
+    contentElement.select();
+    contentElement.focus();
+  } else {
+    selectedItem = null;
+    selectedId = null;
+    updateContent();
+  }
+}
+
+function updateContent() {
+  if (changeTimeoutId !== null) {
+    markChangesAsCommitted();
+  }
+  let contentElement = document.querySelector(".content");
+  let titleElement = document.querySelector(".title");
+
+  if (selectedItem === null) {
+    contentElement.value = "";
+    contentElement.disabled = true;
+    titleElement.value = "";
+    titleElement.disabled = true;
+  } else {
+    contentElement.value = selectedItem.content;
+    contentElement.disabled = false;
+    titleElement.value = selectedItem.title;
+    titleElement.disabled = false;
+  }
+}
+
+function refreshItemList() {
+  itemIds = JSON.parse(localStorage.getItem("items"));
+  let itemsNode = document.querySelector(".items_pane");
+  let focusedIndex = null;
+  if (document.activeElement !== null && document.activeElement.parentNode === itemsNode) {
+    focusedIndex = Array.prototype.indexOf.call(itemsNode.childNodes, document.activeElement);
+  }
+  while (itemsNode.firstChild) {
+    itemsNode.removeChild(itemsNode.firstChild);
+  }
+
+  for (let i = 0; i < itemIds.length; i++) {
+    let id = itemIds[i];
+    let element = createListItem(id, JSON.parse(localStorage.getItem("item:" + id)), i + 3);
+    itemsNode.appendChild(element);
+  }
+  
+  if (focusedIndex !== null) {
+    log("fi " + focusedIndex + " " + itemsNode.childNodes.length);
+    itemsNode.childNodes[focusedIndex].focus();
+  }
+}
+
+function newItem() {
+  let id = generateId();
+  let item = { title: "New", content: "" };
+  localStorage.setItem("item:" + id, JSON.stringify(item));
+  itemIds.push(id);
+  localStorage.setItem("items", JSON.stringify(itemIds));
+ 
+  refreshItemList();
+  selectItem(id);
+
+  let titleElement = document.querySelector(".title");
+  titleElement.select();
+  titleElement.focus();
+}
+
+function deleteCurrentItem() {
+  if (selectedId === null) {
+    return;
+  }
+
+  selectItem(null);
+  let idx = itemIds.indexOf(selectedId);
+  itemIds.splice(idx, 1);
+  localStorage.removeItem("item:" + selectedId);
+  localStorage.setItem("items", JSON.stringify(itemIds));
+  refreshItemList();
+}
+
+function onLoad() {
+  navigator.serviceWorker.register("service-worker.js", { scope: "./" })
+      .then(r => {
+        log("service worker installed: " + r);
+      })
+      .catch(e => {
+        log("could not install service worker: " + e);
+      });
+  refreshItemList();
+}
+
+window.addEventListener("load", onLoad);
---
+++ service-worker.js
@@ -0,0 +1,169 @@
+"use strict";
+
+let FILE_LIST = [
+    "/index.html",
+    "/scratchpad.css",
+    "/scratchpad.js",
+    "/service-worker.js",
+];
+
+let CACHE_NAME = "v1";
+let REFRESH_DELAY = 5000;
+let cachedFiles = {};
+
+function log(s) {
+  console.log(new Date().toISOString() + " " + s);
+}
+
+function installEvent(e) {
+  log("installing " + e);
+  let installedFiles = [];
+  for (let path in cachedFiles) {
+    let request = new Request(path);
+    installedFiles.push(fetch(request).then(response => {
+      return updateCache(path, request.clone(), response);
+    }).catch(err => {
+      log("installation failed for " + path + ": " + e);
+    }));
+  }
+  e.waitUntil(Promise.all(installedFiles).then(() => {
+    log("all files installed");
+  }));
+}
+
+function updateCache(path, request, response) {
+  let result = caches.open(CACHE_NAME).then(cache => {
+    log("Adding cached value for " + path);
+    cache.put(path, response);
+  });
+  let cacheObj = cachedFiles[path];
+  if (cacheObj.timeout) {
+    clearTimeout(cacheObj.timeout);
+  }
+  cacheObj.response = response;
+  cacheObj.stale = false;
+  cacheObj.timeout = setTimeout(() => {
+    log("cache entry expired for " + path);
+    cacheObj.stale = true;
+    cacheObj.timeout = null; }, REFRESH_DELAY);
+  return result;
+}
+
+function fetchEvent2(e) {
+  let path = new URL(e.request.url).pathname;
+  log("async fetch event for " + e.request.url + " " + path);
+  e.respondWith(spawn(function *() {
+    if (!path in cachedFiles) {
+      log("uncached path " + path);
+      yield fetch(e.request);
+    }
+    
+    if (cachedFiles[path].stale) {
+      log("stale path " + path);
+      let fetchResponse;
+      try {
+        fetchResponse = yield fetch(e.request);
+      } catch (err) {
+        log("cannot fetch: " + err);
+        return caches.match(e.request.clone());
+      }
+      if (fetchResponse && fetchResponse.status == 200) {
+        log("updating cache for stale file " + path);
+        updateCache(path, e.request.clone(), fetchResponse.clone());
+        return Promise.resolve(fetchResponse);
+      } else {
+        log("returning cache entry for stale file " + path);
+        return caches.match(e.request.clone());
+      }
+    } else {
+      log("unstale path " + path);
+      let cacheResponse;
+      try {
+        cacheResponse = yield caches.match(e.request);
+      } catch (err) {
+        log("cannot get from cache: " + err);
+        let fetchResponse = yield fetch(e.request.clone());
+        if (fetchResponse && fetchResponse.status == 200) {
+          log("updating cache for unstale file " + path);
+          updateCache(path, e.request.clone(), fetchResponse.clone());
+        }
+        return fetchResponse;
+      }
+      
+      return Promise.resolve(cacheResponse);
+    }
+  }));
+}
+
+function fetchEvent(e) {
+  let path = new URL(e.request.url).pathname;
+  log("fetch event for " + e.request.url + " " + path);
+  if (!path in cachedFiles) {
+    log("uncached path " + path);
+    // We don't want to cache this file
+    e.respondWith(fetch(e.request));
+  }
+  
+  if (cachedFiles[path].stale) {
+    log("stale path " + path);
+    // The file is stale. Try the network, and if it fails, use the cache.
+    e.respondWith(fetch(e.request).then(fetchResponse => {
+      if (fetchResponse && fetchResponse.status == 200) {
+        log("updating cache for stale file " + path);
+        updateCache(path, e.request.clone(), fetchResponse.clone());
+        return fetchResponse;
+      } else {
+        return caches.match(e.request.close());
+      }
+    }).catch(err => {
+      log("fetch failed for " + path + ": " + err);
+      return caches.match(e.request.clone());
+    }));
+  } else {
+    // The file is not stale. Try the cache, and if it's not there, ask the network.
+    log("valid path " + path);
+    e.respondWith(caches.match(e.request).then(cacheMatch => {
+      log("cache hit for " + path);
+      return cacheMatch;
+    }).catch(err => {
+      log("cache error for " + path + ": " + err);
+      fetch(e.request.clone()).then(fetchResponse => {
+        if (fetchResponse && fetchResponse.status == 200) {
+          log("populating cache from valid server response for " + path);
+          updateCache(path, e.request.clone(), fetchResponse.clone());
+        }
+        log("responding for " + path);
+        return response;
+      });
+    }));
+  }
+}
+
+function spawn(generatorFunc) {
+  function continuer(verb, arg) {
+    var result;
+    try {
+      result = generator[verb](arg);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+    if (result.done) {
+      return result.value;
+    } else {
+      return Promise.resolve(result.value).then(onFulfilled, onRejected);
+    }
+  }
+  var generator = generatorFunc();
+  var onFulfilled = continuer.bind(continuer, "next");
+  var onRejected = continuer.bind(continuer, "throw");
+  return onFulfilled();
+}
+
+log("Service worker JS file running");
+
+for (let i = 0; i < FILE_LIST.length; i++) {
+  cachedFiles[FILE_LIST[i]] = { stale: true, response: null };
+}
+
+this.addEventListener("install", installEvent);
+this.addEventListener("fetch", fetchEvent2);
